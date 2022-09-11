d2e33f59ded134c83fa6e806758ba6f3700bbb30
Ivo Pugliese <elsoft74@gmail.com>
2022-09-11 07:40:22 +0200

    update moment

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <script type="text/javascript" src="js/assistito.js"></script>
     <script type="text/javascript" src="js/usca.js"></script>
     <script type="text/javascript" src="js/swabs.js"></script>
-    <script type="text/javascript" src="js/sweetalert2@11"></script>
+    <script type="text/javascript" src="js/sweetalert2.all.min.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.29/dist/sweetalert2.all.min.js"></script> -->
 
 </head>
