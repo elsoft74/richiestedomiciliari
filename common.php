fd3fe7fc998a522f355398bd9cca3035872901ca
Ivo Pugliese <ivo@ivopugliese.it>
2022-12-14 20:15:45 +0100

    Title in head

--- common.php
+++ common.php
@@ -1,8 +1,9 @@
 <?php
 
-function buildHead(){
+function buildHead($title){
 
 echo('<head>');
+echo('<title>'.$title.'</title>');
 echo('<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">');
 echo('<script src="bootstrap/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"></script>');
 echo('<script type="text/javascript" src="js/xlsx.full.min.js"></script>');
